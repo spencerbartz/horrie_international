2536d7e5e14d41fb6aab1d2a6312ac52e250be52
spencerbartz <spencerbartz@gmail.com>
2016-01-19 22:22:33 -0800

    clean up js a bit, still pretty messy

--- util/js/util.js
+++ util/js/util.js
@@ -4,81 +4,11 @@ Javascript utility functions for horrieinternational.com
 *
 *************************util.js*************************/
 
-function checkFormFields(formToCheck, errorDiv, ignoreList)
+function promptDelete(filename, object, redirectUrl)
 {
-    //Thanks I.E. for not allowing default values... Set it to null just so we know what the value is
-    if(!ignoreList)
-        ignoreList = null;
-    var myForm = document.getElementById(formToCheck);
-    var myDiv = document.getElementById(errorDiv);
-    var msg = "";
-    var elmnts = myForm.elements;
-    var ignore = !ignoreList ? new Array() : ignoreList;
-
-    //console.log("ignore :" + ignore.toString());
-    if(ignoreList != null)
-            console.log("ignore list :" + ignoreList.toString());
-    //console.log("index of middlename " + ignore.indexOf("middlename"));
-
-    //Reset the display colors for each input name (if one or more was red, turn it white)
-    for(var i = 0; i < elmnts.length; i++)
+    if (confirm("Do you really want to delete this " + object.toString() + "?"))
     {
-        var field = document.getElementById("err" + elmnts[i].name);
-        if(field)
-            field.style.color = "white";
-    }
-
-    //Check to see if any textfields or textareas are blank
-    for(var i = 0; i < elmnts.length; i++)
-    {
-        if(elmnts[i].type == "text" || elmnts[i].type == "textarea")
-        {
-                if(elmnts[i].value == "" && ignore.indexOf(elmnts[i].name) < 0)
-                {
-                        msg = "Error: you must fill in the <strong>" + elmnts[i].name + "</strong> field";
-                        myDiv.style.display = "inline";
-                        myDiv.innerHTML = msg;
-
-                        var field = document.getElementById("err" + elmnts[i].name);
-                        field.style.color = "red";
-
-                        return false;
-                }
-
-                if(elmnts[i].name == "email")
-                {
-                        //alert(/[_a-zA-Z0-9\.-]+?@[_a-zA-Z0-9\.-]+?\.[a-zA-Z]{2,4}$/.test(elmnts[i].value));
-
-                        //regex for checking email format
-                        if(/^[_a-zA-Z0-9.-]+@[_a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/.test(elmnts[i].value) == false)
-                        {
-                                msg = "Error: Invalid email format";
-                                myDiv.style.display = "inline";
-                                myDiv.innerHTML = msg;
-
-                                var field = document.getElementById("err" + elmnts[i].name);
-                                field.style.color = "red";
-
-                                return false;
-                        }
-
-                }
-
-                if(elmnts[i] == "message")
-                {
-                        //Do sanitization here!
-                }
-        }
-    }
-
-    return true;
-}
-
-function promptDeletePhoto(filename, photoid)
-{
-    if (confirm("Do you really want to delete this photo?"))
-    {
-        window.location.assign("deletephoto.php?dbname=" + filename + "&id=" + photoid);
+        window.location.assign(redirectUrl);
     }
 }
 
@@ -108,11 +38,11 @@ function deactivateSearch()
 {
     if(searchActive)
     {
-            hideOverlay();
-            moveDiv('searchbox', getCurrentCoords('searchbox'), getStartCoords());
-            hideCloseButton();
-            searchActive = false;
-            playSound("../sounds/bwip.mp3");
+        hideOverlay();
+        moveDiv('searchbox', getCurrentCoords('searchbox'), getStartCoords());
+        hideCloseButton();
+        searchActive = false;
+        playSound("../sounds/bwip.mp3");
     }
 }
 
@@ -160,63 +90,63 @@ function moveDiv(divId, startCoords, endCoords)
     }
     else
     {
-            var offset = targetDiv.offset();
-            
-            startX = Math.round(offset.left);
-            startY = Math.round(offset.top);
-            
-            var endX = endCoords.x; 
-            var endY = endCoords.y; 
-            
-            var steps = 10;
+        var offset = targetDiv.offset();
+        
+        startX = Math.round(offset.left);
+        startY = Math.round(offset.top);
+        
+        var endX = endCoords.x; 
+        var endY = endCoords.y; 
+        
+        var steps = 10;
 
-            var curX = startX;
-            var curY = startY;
-            
-            var movDistX = Math.abs(endX - startX) / steps;
-            var movDistY = Math.abs(endY - startY) / steps;
-                       
-            var id = setInterval(function() {
-                if(startX < endX) {
-                    if((curX + movDistX) > endX) {
-                        curX = endX;
-                    }
-                    else {
-                        curX += movDistX;
-                    }
+        var curX = startX;
+        var curY = startY;
+        
+        var movDistX = Math.abs(endX - startX) / steps;
+        var movDistY = Math.abs(endY - startY) / steps;
+                   
+        var id = setInterval(function() {
+            if(startX < endX) {
+                if((curX + movDistX) > endX) {
+                    curX = endX;
                 }
-                else if(startX > endX) {
-                    if((curX - movDistX) < endX) {
-                        curX = endX;
-                    }
-                    else {
-                        curX -= movDistX;
-                    }				
+                else {
+                    curX += movDistX;
                 }
-                            
-                if(startY < endY) {
-                    if((curY + movDistY) > endY) {
-                        curY = endY;
-                    }
-                    else {
-                        curY += movDistY;
-                    }
+            }
+            else if(startX > endX) {
+                if((curX - movDistX) < endX) {
+                    curX = endX;
                 }
-                else if(startY > endY) {
-                    if((curY - movDistY) < endY) {
-                        curY = endY;
-                    }
-                    else {
-                        curY -= movDistY;
-                    }				
+                else {
+                    curX -= movDistX;
+                }				
+            }
+                        
+            if(startY < endY) {
+                if((curY + movDistY) > endY) {
+                    curY = endY;
                 }
-                    
-                if (curX == endX && curY == endY){
-                        clearInterval(id);
+                else {
+                    curY += movDistY;
                 }
-                    
-                targetDiv.offset({left: Math.round(curX), top: Math.round(curY)});
-            }, 20);
+            }
+            else if(startY > endY) {
+                if((curY - movDistY) < endY) {
+                    curY = endY;
+                }
+                else {
+                    curY -= movDistY;
+                }				
+            }
+                
+            if (curX == endX && curY == endY){
+                    clearInterval(id);
+            }
+                
+            targetDiv.offset({left: Math.round(curX), top: Math.round(curY)});
+        }, 20);
     }
 }
 
@@ -227,26 +157,26 @@ function moveDiv(divId, startCoords, endCoords)
  */
 function showOverlay()
 {
-    var el = document.getElementById("overlay");
+    var el = _el("overlay");
     el.style.visibility = "visible";
 }
 
 function hideOverlay()
 {
     active = false;
-    var el = document.getElementById("overlay");
+    var el = _el("overlay");
     el.style.visibility = "hidden";
 }
 
 function showCloseButton()
 {
-    var el = document.getElementById("close-button");
+    var el = _el("close-button");
     el.style.display = "block";
 }
 
 function hideCloseButton()
 {
-    var el = document.getElementById("close-button");
+    var el = _el("close-button");
     el.style.display = "none";
 }
 
@@ -276,7 +206,7 @@ function validateTextAreaInput(textAreaId)
 
 function resetForm()
 {
-    el("posttext").innerHTML = "";
+    _el("posttext").innerHTML = "";
 }
 
 function _el(elem)
